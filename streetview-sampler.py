bb38ced967f2cbfb6412044339e1f64fd06cc683
Matthew Van Brummelen <vb.matthew@gmail.com>
2023-10-24 21:36:57 -0700

    added url signature

--- streetview-sampler.py
+++ streetview-sampler.py
@@ -8,13 +8,13 @@ from tqdm import tqdm
 
 load_dotenv()
 GOOGLE_MAPS_API_KEY = os.getenv('GOOGLE_MAPS_API_KEY')
+URL_SIGNING_SECRET = os.getenv('URL_SIGNING_SECRET')
 
 # People per km^2
 # Targetting suburban areas
 MAX_DENSITY = 5500
 MIN_DENSITY = 1000
 MIN_POPULATION = 20000
-
 
 with open('north-america-cities.csv', 'r') as csvfile:
     reader = csv.DictReader(csvfile)
@@ -40,17 +40,16 @@ print("first 50 cities: ", [city["city_ascii"]
 results = []
 for city in high_pop_cities:
     # Construct the URL for the Street View API
-    url = f"https://maps.googleapis.com/maps/api/streetview/metadata?location={city['lat']},{city['lng']}&key={GOOGLE_MAPS_API_KEY}&signature="
+    url = f"https://maps.googleapis.com/maps/api/streetview/metadata?location={city['lat']},{city['lng']}&key={GOOGLE_MAPS_API_KEY}&signature={URL_SIGNING_SECRET}"
 
     response = requests.get(url, stream=True)
     if response.status_code == 200:
-        # Save image to local storage (optional)
-        # with open(f"{city['name']}.jpg", 'wb') as f:
-        #     for chunk in response:
-        #         f.write(chunk)
-
         # Append to results
         results.append({"City": city['name'], "ImageURL": url})
+
+    else:
+        raise Exception(
+            f"Error retrieving Street View metadata for: {city['city_ascii']}")
 
 # Convert results to a DataFrame and save to Excel
 df = pd.DataFrame(results)
