5c765a3db4fee71a04ef6642ed98d6794b483019
Matthew Van Brummelen <vb.matthew@gmail.com>
2023-10-24 19:15:52 -0700

    adding constraints

--- streetview-sampler.py
+++ streetview-sampler.py
@@ -9,6 +9,12 @@ from tqdm import tqdm
 load_dotenv()
 GOOGLE_MAPS_API_KEY = os.getenv('GOOGLE_MAPS_API_KEY')
 
+# People per km^2
+# Targetting suburban areas
+MAX_DENSITY = 5500
+MIN_DENSITY = 1000
+MIN_POPULATION = 20000
+
 ### EACH RUN OF THIS SCRIPT WILL USE ~2500 API REQUESTS COSTING ~$18 USD ###
 
 with open('north-america-cities.csv', 'r') as csvfile:
@@ -16,7 +22,7 @@ with open('north-america-cities.csv', 'r') as csvfile:
     high_pop_cities = []
 
     for row in tqdm(reader):
-        if int(row["population"]) > 20000:
+        if int(row["population"]) > MIN_POPULATION and float(row["density"]) > MIN_DENSITY and float(row["density"]) < MAX_DENSITY:
             high_pop_cities.append({
                 "city_ascii": str(row["city_ascii"]),
                 "region_id": str(row["region_id"]),
