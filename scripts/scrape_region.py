5196902e5337ae1bf0f3d1a96e179f1db3ba7ad8
Matthew Van Brummelen <vb.matthew@gmail.com>
2023-10-31 18:31:09 -0700

    corrections to logic

--- scripts/scrape_region.py
+++ scripts/scrape_region.py
@@ -1,4 +1,5 @@
 from utils import meters_to_degrees
+from utils import request_metadata
 from utils import request_streetview
 
 
@@ -6,13 +7,26 @@ def scrape_region(lat_max, lat_min, lng_max, lng_min, meter_step):
     step = meters_to_degrees(meter_step)
 
     # Scan metadata for each location and record points with streetview
+    locations = []
     for lat in range(lat_min, lat_max, step):
         for lng in range(lng_min, lng_max, step):
             parameters = {
                 "location": f"{lat},{lng}",
-                "size": "640x400",
-                "fov": "120",
-                "pitch": "0",
+                "radius": meter_step*1.5
             }
-            request_streetview(
-                parameters, f"../data/images/{lat}_{lng}.jpg")
+            response = request_metadata(
+                parameters)
+            locations.append(
+                ((response["location"]["lat"]), response["location"]["lng"]))
+
+    locations = list(set(locations))
+    # Request image for each location and save to file
+    for location in locations:
+        parameters = {
+            "location": f"{location[0]},{location[1]}",
+            "size": "640x400",
+            "fov": "120",
+            "pitch": "0",
+        }
+        request_streetview(
+            parameters, f"../data/images/{location[0]}_{location[1]}.jpg")
