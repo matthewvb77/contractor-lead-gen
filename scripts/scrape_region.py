9124486fc2b14786def3a35b414db97814ed29d0
Matthew Van Brummelen <vb.matthew@gmail.com>
2023-10-31 19:06:54 -0700

    adding data validation

--- scripts/scrape_region.py
+++ scripts/scrape_region.py
@@ -4,6 +4,15 @@ from utils import request_streetview
 
 
 def scrape_region(lat_max, lat_min, lng_max, lng_min, meter_step):
+    # Data validation
+    assert lat_max > lat_min
+    # assert lng_max > lng_min ---> NOT TRUE FOR CROSSING THE INTERNATIONAL DATE LINE
+    assert meter_step > 0
+    assert lat_max > -90 and lat_max < 90
+    assert lat_min > -90 and lat_min < 90
+    assert lng_max > -180 and lng_max < 180
+    assert lng_min > -180 and lng_min < 180
+
     step = meters_to_degrees(meter_step)
 
     # Scan metadata for each location and record points with streetview
@@ -38,8 +47,8 @@ def scrape_region(lat_max, lat_min, lng_max, lng_min, meter_step):
 
         params["heading"] = 180
         request_streetview(
-            params, f"../images/{location}_E.jpg")
+            params, f"../images/{location}_S.jpg")
 
         params["heading"] = 270
         request_streetview(
-            params, f"../images/{location}_E.jpg")
+            params, f"../images/{location}_W.jpg")
