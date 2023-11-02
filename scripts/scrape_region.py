2fb9ff2e8c28e4d68e1c74be95849419b89f588a
Matthew Van Brummelen <vb.matthew@gmail.com>
2023-11-02 14:54:45 -0700

    better path creation for cross-os use

--- scripts/scrape_region.py
+++ scripts/scrape_region.py
@@ -19,7 +19,7 @@ def scrape_region(lat_min, lat_max, lng_min, lng_max, meter_step):
 
     # Create folder and logs for script results
     run_id = uuid.uuid4()
-    folder_path = f'../data/region_scrape_{run_id}'
+    folder_path = os.path.join('..', 'data', f'region_scrape_{run_id}')
     if not os.path.exists(folder_path):
         os.mkdir(folder_path)
     else:
